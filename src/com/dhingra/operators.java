9ebddd622eb7ff9a98e926793037ef3140dd2393
tdhingra03 <tejdhingra@gmail.com>
2020-06-18 15:42:24 -0500

    commit

--- src/com/dhingra/operators.java
+++ src/com/dhingra/operators.java
@@ -1,4 +1,20 @@
-package com.dhingra;
+//import scanner
+//create scanner object
+//define variables, mealCost, tipPercent, taxPercent
+//println input meal cost
+//use nextDouble to initialize variable for meal cost
+//println input tip percent
+//use nextInt to initialize variable for tip percent
+//println input tax percent
+//use nextInt to initialize variable for tax percent
+//create new tip variable which is total cost of meal times tip percent
+//^^do the same for tax
+//define variable of total cost to be the meal cost plus tip and tax
+//make sure to use Math.round to round total cost to nearest whole number
+//println what the total cost of the meal is
+
+
+        package com.dhingra;
 
 import java.util.*;
 
@@ -19,8 +35,7 @@ public class operators {
 
         double tip = mealCost * tipPercent / 100;
         double tax = mealCost * taxPercent / 100;
-
-        int totalCost = (int) Math.round(mealCost + tip + tax);
+        double totalCost = (int) Math.round(mealCost + tip + tax);
 
         System.out.println("The total meal cost is " + totalCost + " dollars.");
     }
